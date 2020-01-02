0804ca3c933b894d0ffaf7cc3c44c437e1e4d0eb
dedederek <lihang@starwit.com>
2020-01-02 13:28:43 +0800

    Update extension.ts

--- extension.ts
+++ extension.ts
@@ -33,6 +33,15 @@ namespace MC102EXT {
         }
     }
 
+    export function sayhi() {
+        mobs.execute(
+            mobs.target(TargetSelectorKind.NearestPlayer),
+            positions.create(0, 0, 0),
+            "say hi"
+        )
+    }
+
+    
     /**
      * Opens a gate
      */
